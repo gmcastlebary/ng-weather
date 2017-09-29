4271231388bb93b3f801aad93a226d3667dfbed5
gabeca <gabe.castlebary@vsp.com>
2017-09-29 14:10:10 -0700

    passing zip code down to local weather component

--- src/app/local-weather/local-weather.component.ts
+++ src/app/local-weather/local-weather.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-local-weather',
@@ -7,8 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LocalWeatherComponent implements OnInit {
 
-  constructor() {
+  @Input()
+  zip: string;
 
+  constructor() {
   }
 
   ngOnInit() {
